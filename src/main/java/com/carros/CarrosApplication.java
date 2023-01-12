0c6700240961a8a2027440a4f7de5961fc8e697c
CarlosHolz <carlos.holz07@gmail.com>
2023-01-12 18:03:48 -0300

    Métodos de post, put e delete

--- src/main/java/com/carros/CarrosApplication.java
+++ src/main/java/com/carros/CarrosApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { ErrorMvcAutoConfiguration.class })
