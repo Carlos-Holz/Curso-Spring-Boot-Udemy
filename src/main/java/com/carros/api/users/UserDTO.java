0e163788b93751ec89aee667f1e2f7d384c13a25
CarlosHolz <carlos.holz07@gmail.com>
2023-01-18 11:59:49 -0300

    Pacote de usuários e suas respectivas classes de roles, permissões, entre outras...

--- src/main/java/com/carros/api/users/UserDTO.java
+++ src/main/java/com/carros/api/users/UserDTO.java
@@ -1,29 +1,44 @@
-package com.carros.domain.dto;
+package com.carros.api.users;
 
-import com.carros.domain.User;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDTO {
-    private String login;
-    private String nome;
-    private String email;
+	private String login;
+	private String nome;
+	private String email;
 
-    // token jwt
-    private String token;
+	// token jwt
+	private String token;
 
-    public static UserDTO create(User user, String token) {
-        ModelMapper modelMapper = new ModelMapper();
-        UserDTO dto = modelMapper.map(user, UserDTO.class);
-        dto.token = token;
-        return dto;
-    }
+	private List<String> roles;
 
-    public String toJson() throws JsonProcessingException {
-        ObjectMapper m = new ObjectMapper();
-        return m.writeValueAsString(this);
-    }
+	public static UserDTO create(User user) {
+		ModelMapper modelMapper = new ModelMapper();
+		UserDTO dto = modelMapper.map(user, UserDTO.class);
+
+		dto.roles = user.getRoles().stream().map(Role::getNome).collect(Collectors.toList());
+
+		return dto;
+	}
+
+	public static UserDTO create(User user, String token) {
+		UserDTO dto = create(user);
+		dto.token = token;
+		return dto;
+	}
+
+	public String toJson() throws JsonProcessingException {
+		ObjectMapper m = new ObjectMapper();
+		return m.writeValueAsString(this);
+	}
 }
