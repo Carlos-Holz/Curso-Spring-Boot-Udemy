0a7de3fbbf1cbf2a36b0913c2af94cd73c38fad1
CarlosHolz <carlos.holz07@gmail.com>
2023-01-18 11:58:15 -0300

    Classes de Security Config e UserDetails para segurança de usuário e requisições

--- src/main/java/com/carros/api/infra/SecurityConfig.java
+++ src/main/java/com/carros/api/infra/SecurityConfig.java
@@ -1,9 +1,9 @@
-package com.carros.api.security;
+package com.carros.api.infra;
 
-import com.carros.api.security.jwt.JwtAuthenticationFilter;
-import com.carros.api.security.jwt.JwtAuthorizationFilter;
-import com.carros.api.security.jwt.handler.AccessDeniedHandler;
-import com.carros.api.security.jwt.handler.UnauthorizedHandler;
+import com.carros.api.infra.security.jwt.JwtAuthenticationFilter;
+import com.carros.api.infra.security.jwt.JwtAuthorizationFilter;
+import com.carros.api.infra.security.jwt.handler.AccessDeniedHandler;
+import com.carros.api.infra.security.jwt.handler.UnauthorizedHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
@@ -23,34 +23,42 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	@Qualifier("userDetailsService")
-	private UserDetailsService userDetailsService;
+    @Autowired
+    @Qualifier("userDetailsService")
+    private UserDetailsService userDetailsService;
 
-	@Autowired
-	private UnauthorizedHandler unauthorizedHandler;
+    @Autowired
+    private UnauthorizedHandler unauthorizedHandler;
 
-	@Autowired
-	private AccessDeniedHandler accessDeniedHandler;
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		AuthenticationManager authManager = authenticationManager();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        AuthenticationManager authManager = authenticationManager();
 
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1/login").permitAll()
-				.antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
-				.anyRequest().authenticated().and().csrf().disable().addFilter(new JwtAuthenticationFilter(authManager))
-				.addFilter(new JwtAuthorizationFilter(authManager, userDetailsService)).exceptionHandling()
-				.accessDeniedHandler(accessDeniedHandler).authenticationEntryPoint(unauthorizedHandler).and()
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-	}
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/v1/login").permitAll()
+                .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+                .addFilter(new JwtAuthenticationFilter(authManager))
+                .addFilter(new JwtAuthorizationFilter(authManager, userDetailsService))
+                .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler)
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-		auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
-	}
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    }
 
 }
--- src/main/java/com/carros/api/infra/UserDetailsServiceImpl.java
+++ src/main/java/com/carros/api/infra/UserDetailsServiceImpl.java
@@ -1,12 +1,13 @@
-package com.carros.api.security;
+package com.carros.api.infra;
 
-import com.carros.domain.User;
-import com.carros.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import com.carros.api.users.User;
+import com.carros.api.users.UserRepository;
 
 @Service(value = "userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
