715f6cbeb7fea2a21f0784881e39cd52a6f48f66
CarlosHolz <carlos.holz07@gmail.com>
2023-01-18 11:59:00 -0300

    Pacote upload com todas as respectivas classes de Swagger, Firebase, UploadController, dentre outras...

--- src/main/java/com/carros/api/upload/IndexController.java
+++ src/main/java/com/carros/api/upload/IndexController.java
@@ -1,7 +1,5 @@
-package com.carros.api;
+package com.carros.api.upload;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +11,5 @@ public class IndexController {
 	@GetMapping()
 	public String get() {
 		return "API dos Carros";
-	}
-
-	@GetMapping("/userInfo")
-	public UserDetails userInfo(@AuthenticationPrincipal UserDetails user) {
-		return user;
 	}
 }
